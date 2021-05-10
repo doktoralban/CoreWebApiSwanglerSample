67a45f50ac7873a1108c73a84e69a8bb234b122c
Aydın Türker <doktoralban@outlook.com>
2021-05-10 22:31:25 +0300

    ADDED return ANONYMOUS type -Task<IActionResult>-

--- WebApplication1/Controllers/ProductsController.cs
+++ WebApplication1/Controllers/ProductsController.cs
@@ -1,13 +1,13 @@
 ﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Threading.Tasks;
 using WebApplication1.Models;
 using WebApplication1.Utils;
+ 
 
 namespace WebApplication1.Controllers
 {
@@ -34,6 +34,48 @@ namespace WebApplication1.Controllers
             string sql = "SELECT * FROM dbo.PRODUCTS";
             return await connection.QueryAsync<Product>(sql);
         }
+        /// <summary>
+        /// FirstOrDefault
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("EnUcuz")]
+        public async Task<Product> EnUcuz()
+        {
+            string sql = "SELECT TOP 1 * FROM dbo.PRODUCTS WHERE UnitPrice >0 ORDER BY UnitPrice ASC ; ";
+            return await connection.QueryFirstOrDefaultAsync<Product>(sql); 
+        }
+
+        /// <summary>
+        /// FirstOrDefault
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("EnPahali")]
+        public async Task<Product> EnPahali()
+        {
+            string sql = "SELECT TOP 1 * FROM dbo.PRODUCTS WHERE UnitPrice >0 ORDER BY UnitPrice DESC ; ";
+            return await connection.QueryFirstOrDefaultAsync<Product>(sql);
+        }
+
+        /// <summary>
+        /// return ANONYMOUS type
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("OrdersAndDetails")]
+        [ProducesResponseType(StatusCodes.Status200OK )]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> OrdersAndDetails()
+        {
+            string sql = "SELECT * FROM vOrdersAndDetails";
+
+            var result =await connection.QueryAsync(sql);
+
+            return Ok(result);
+        }
+
+       
 
 
 
